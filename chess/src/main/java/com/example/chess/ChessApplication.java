7e5eed6efb7e2600128593fd3002af6de95d5698
Jason Gillanders <jgilland@ualberta.ca>
2026-04-30 17:16:17 -0600

    create spring boot, trying to get CLI working

---
+++ chess/src/main/java/com/example/chess/ChessApplication.java
@@ -0,0 +1,12 @@
+package com.example.chess;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessApplication {
+
+	public static void main(String[] args) throws Exception {
+		org.springframework.boot.SpringApplication.run(ChessApplication.class, args);
+	}
+
+}
---
+++ chess/src/main/java/com/example/chess/GreetingCommands.java
@@ -0,0 +1,23 @@
+package com.example.chess;
+
+import org.springframework.stereotype.Component;
+import org.springframework.shell.core.command.annotation.Command;
+import org.springframework.shell.core.command.annotation.Option;
+
+@Component
+public class GreetingCommands {
+
+	@Command(name = "hello", description = "Say hello to a given name", group = "Greetings",
+			help = "A command that greets the user with 'Hello ${name}!'. Usage: hello [-n | --name]=<name>")
+	public void sayHello(
+		@Option(
+			shortName = 'n',
+			longName = "name",
+			description = "the name of the person to greet",
+			defaultValue = "World"
+		)
+		String name) {
+		System.out.println("Hello " + name + "!");
+	}
+
+}
